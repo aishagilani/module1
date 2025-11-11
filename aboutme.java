df661497ffe8efa41425a24c2273b3e1e8071123
aisha gilani <aishaagilani@gmail.com>
2025-11-11 18:53:26 +0530

    Create aboutme.java

---
+++ aboutme.java
@@ -0,0 +1,10 @@
+public class aboutme {
+   public static void main(String[] args) {
+       String name = "Aisha";
+       int age = 20;
+       String language = "C";
+       System.out.println("Name: " + name);
+       System.out.println("Age: " + age);
+       System.out.println("Favorite Language: " + language);
+   }
+}
